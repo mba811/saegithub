5646e84ee61cad7ea4e59972aec1689859537a1c
stigliew <stigliew@gmail.com>
2012-03-30 10:53:36 +0800

    test commit 2

--- githubXweibo/config.php
+++ githubXweibo/config.php
@@ -2,8 +2,8 @@
 define( "GITHUB_USER" , '' );
 define( "GITHUB_PASS" , '' );
 
-define( "WB_AKEY" , '2498151062' );
-define( "WB_SKEY" , '0d62162faee3be88fe7a9c3ab9a98289' );
-define( "WB_OAUTH2" , '2.00RGxnxBENzDjC7b4013d1ca03jdcO' );
+define( "WB_AKEY" , '' );
+define( "WB_SKEY" , '' );
+define( "WB_OAUTH2" , '' );
 
 define( "KVDB_PERFIX" , 'SAE_GH' );
