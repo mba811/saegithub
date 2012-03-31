d4de0f64db13d34e83ba8203cf4d71858b772277
stigliew <stigliew@gmail.com>
2012-03-31 10:57:26 +0800

    github action timeline to weibo

---
+++ githubXweibo/actionsync.php
@@ -0,0 +1,53 @@
+<?php
+	include_once( 'config.php' );
+	include_once( 'saetv2.ex.class.php' );
+	$c = new SaeTClientV2( WB_AKEY , WB_SKEY , WB_OAUTH2);
+	$uid_get = $c->get_uid();
+	$uid = $uid_get['uid'];
+	
+	$tempurl='https://api.github.com/users/stigliew/events/public';
+	$f = new SaeFetchurl();
+	$kv = new SaeKV();
+	$kv->init();
+	$content = $f->fetch($tempurl);
+	$content = array_reverse (json_decode($content, true));
+	
+	foreach( $content as $key => $value){
+		$message='';
+		$lastcheck = $kv->get('WB_GH_SYNC_'.$value['id']);
+		if($lastcheck === false){
+			$kv->add('WB_GH_SYNC_'.$value['id'], 0);
+		}else{
+			continue;
+		}
+		$value['repo']['url']=str_replace('api.github.com/repos','github.com',$value['repo']['url']);
+		if($value['type']=='IssuesEvent'){
+			die('one round over');
+		}else if($value['type']=='PushEvent'){
+			$url=$value['payload']['commits'][0]['url'];
+			$message=$value['payload']['commits'][0]['message'];
+			$message="pushed to ".$value['payload']['ref']." at ".$value['repo']['name']." ,commits:".$message." ,url:".$url;
+			echo $message.'<br />';
+			$c->update( '[Github:event]'.$message );
+			die('one round over');
+		}else if($value['type']=='CreateEvent'){
+			$message="created ".$value['payload']['ref_type'].' '.$value['payload']["master_branch"];
+			$message=$message." at ".$value['repo']['name']." ".$value['repo']['url'];
+			echo $message.'<br />';
+			$c->update( '[Github:event]'.$message );
+			die('one round over');
+		}else if($value['type']=='WatchEvent'){
+			$message="started watching ".$value['repo']['name']." ".$value['repo']['url'];
+			echo $message.'<br />';
+			$c->update( '[Github:event]'.$message );
+			die('one round over');
+		}else if($value['type']=='ForkEvent'){
+			$message="forked ".$value['repo']['name']." ".$value['repo']['url'];
+			echo $message.'<br />';
+			$c->update( '[Github:event]'.$message );
+			die('one round over');
+		}else{
+			echo $value['id'].' '.$value['type'].'<br />';
+		}
+	}
+	die();
