eb9d5df0eccecbf580081831811f75f0b09b8142
stigliew <stigliew@gmail.com>
2012-03-31 12:20:01 +0800

    github octocat to weibo as pic

--- githubXweibo/actionsync.php
+++ githubXweibo/actionsync.php
@@ -1,6 +1,10 @@
 <?php
 	include_once( 'config.php' );
 	include_once( 'saetv2.ex.class.php' );
+	$octocat=explode("|",OCTOCATS);
+	$octocat=$octocat[rand(0,count($octocat)-1)];
+	$octocat="http://octodex.github.com/images/".$octocat;
+	echo $octocat;
 	$c = new SaeTClientV2( WB_AKEY , WB_SKEY , WB_OAUTH2);
 	$uid_get = $c->get_uid();
 	$uid = $uid_get['uid'];
@@ -22,29 +26,29 @@
 		}
 		$value['repo']['url']=str_replace('api.github.com/repos','github.com',$value['repo']['url']);
 		if($value['type']=='IssuesEvent'){
-			die('one round over');
+			continue;
 		}else if($value['type']=='PushEvent'){
 			$url=$value['payload']['commits'][0]['url'];
 			$message=$value['payload']['commits'][0]['message'];
 			$message="pushed to ".$value['payload']['ref']." at ".$value['repo']['name']." ,commits:".$message." ,url:".$url;
 			echo $message.'<br />';
-			$c->update( '[Github:event]'.$message );
+			$c->upload( '#Github:event#'.$message,$octocat );
 			die('one round over');
 		}else if($value['type']=='CreateEvent'){
 			$message="created ".$value['payload']['ref_type'].' '.$value['payload']["master_branch"];
 			$message=$message." at ".$value['repo']['name']." ".$value['repo']['url'];
 			echo $message.'<br />';
-			$c->update( '[Github:event]'.$message );
+			$c->upload( '#Github:event#'.$message,$octocat );
 			die('one round over');
 		}else if($value['type']=='WatchEvent'){
 			$message="started watching ".$value['repo']['name']." ".$value['repo']['url'];
 			echo $message.'<br />';
-			$c->update( '[Github:event]'.$message );
+			$c->upload( '#Github:event#'.$message,$octocat );
 			die('one round over');
 		}else if($value['type']=='ForkEvent'){
 			$message="forked ".$value['repo']['name']." ".$value['repo']['url'];
 			echo $message.'<br />';
-			$c->update( '[Github:event]'.$message );
+			$c->upload( '#Github:event#'.$message,$octocat );
 			die('one round over');
 		}else{
 			echo $value['id'].' '.$value['type'].'<br />';
--- githubXweibo/config.php
+++ githubXweibo/config.php
@@ -1,14 +1,17 @@
 ﻿<?php
-define( "GITHUB_USER" , '' );
-define( "GITHUB_PASS" , '' );
+define( "GITHUB_USER" , 'stigliew' );
+define( "GITHUB_PASS" , 'yako1988' );
 
-define( "WB_AKEY" , '' );
-define( "WB_SKEY" , '' );
-define( "WB_OAUTH2" , '' );
+define( "WB_AKEY" , '2498151062' );
+define( "WB_SKEY" , '0d62162faee3be88fe7a9c3ab9a98289' );
+define( "WB_OAUTH2" , '2.00RGxnxBENzDjC7b4013d1ca03jdcO' );
 
 define( "KVDB_PERFIX" , 'SAE_GH_' );
 
 define("WB_ISSUE_TAG",'issue');
 define("WB_ISSUE_TAG_REPO",'repo');
 define("WB_ISSUE_TAG_TITLE",'title');
+
+define("OCTOCATS",'snowoctocat.jpg|electrocat.png|aidorucat.png|codercat.jpg|strongbadtocat.png|adventure-cat.jpg|doctocat-brown.jpg|dojocat.jpg|defunktocat.png|herme-t-crabb.png|saint-nicktocat.jpg|orderedlistocat.jpg|thanktocat.jpg|megacat.jpg|linktocat.jpg|plumber.jpg|octotron.jpg|baracktocat.jpg|octocat-de-los-muertos.jpg|grim-repo.jpg|father_timeout.jpg|waldocat.jpg|hipster-partycat.jpg|riddlocat.jpg|visionary.jpg|oktobercat.jpg|shoptocat.jpg|nyantocat.gif|octdrey-catburn.jpg|spectrocat.png|bear-cavalry.jpg|andycat.jpg|notocat.jpg|dodgetocat.jpg|cloud.jpg|scarletteocat.jpg|poptocat.jpg|jenktocat.jpg|xtocat.jpg|chellocat.jpg|cherryontop-o-cat.jpg|supportcat.jpg|collabocats.jpg|constructocat2.jpg|total-eclipse-of-the-octocat.jpg|pacman-ghosts.jpg|okal-eltocat.jpg|octoclark-kentocat.jpg|agendacat.jpg|ironcat.jpg|inspectocat.jpg|jean-luc-picat.jpg|spocktocat.jpg|wilson.jpg|swagtocat.jpg|drunktocat.jpg|dolla-dolla-bill-yall.jpg|hubot.jpg|monroe.jpg|trekkie.jpg|octonaut.jpg|bouncer.jpg|founding-father.jpg|pythocat.jpg|drupalcat.jpg|socialite.jpg|setuptocat.jpg|repo.jpg|forktocat.jpg|benevocats.jpg|scottocat.jpg|puppeteer.jpg|octobiwan.jpg|class-act.jpg|original.jpg');
+
 //[issue repo="" title=""][/issue]
